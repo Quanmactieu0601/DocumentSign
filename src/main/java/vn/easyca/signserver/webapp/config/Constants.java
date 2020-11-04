1eca85f93a00cf5d889f00bff197b0e0136d3c11
ThanhLD <duythanh.h3t@gmail.com>
2020-11-04 13:58:13 +0700

    update config hsm

--- src/main/java/vn/easyca/signserver/webapp/config/Constants.java
+++ src/main/java/vn/easyca/signserver/webapp/config/Constants.java
@@ -23,17 +23,23 @@ public final class Constants {
     }
 
     public interface HSMConfig {
-        String NAME = "EasyCAToken";
-        String LIB = "C:\\Windows\\System32\\easyca_csp11_v1.dll";
-        String PIN = "12345678";
-        String SLOT = null;
-        String ATTRIBUTES = null;
+//        String NAME = "EasyCAToken";
+//        String LIB = "C:\\Windows\\System32\\easyca_csp11_v1.dll";
+//        String PIN = "12345678";
+//        String SLOT = null;
+//        String ATTRIBUTES = null;
 
 //        public static final String NAME = "nCipher";
 //        public static final String LIB = "/opt/nfast/toolkits/pkcs11/libcknfast.so";
 //        public static final String PIN = "05111989";
 //        public static final String SLOT = "761406615";
 //        public static final String ATTRIBUTES = "compatibility";
+//
+        public static final String NAME = "ND1_HSM2";
+        public static final String LIB = "/opt/safenet/protecttoolkit5/ptk/lib/libcryptoki.so";
+        public static final String PIN = "Hanoi@202o";
+        public static final String SLOT = "1"; //614352
+        public static final String ATTRIBUTES = "compatibility";
     }
 
     private Constants() {
