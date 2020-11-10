1513ad586a8244264a012dfe34187f0c738bc49b
tungpt <tungpt@softdreams.vn>
2020-11-10 15:25:46 +0700

    move class handle image to Util

--- src/main/java/vn/easyca/signserver/webapp/web/rest/controller/SignController.java
+++ src/main/java/vn/easyca/signserver/webapp/web/rest/controller/SignController.java
@@ -20,6 +20,7 @@ import vn.easyca.signserver.core.dto.sign.request.SignRequest;
 import vn.easyca.signserver.core.dto.sign.response.PDFSigningDataRes;
 import vn.easyca.signserver.core.dto.sign.response.SignDataResponse;
 import vn.easyca.signserver.core.dto.sign.response.SignResultElement;
+import vn.easyca.signserver.core.utils.HtmlImageGeneratorCustom;
 import vn.easyca.signserver.webapp.enm.TransactionType;
 import vn.easyca.signserver.webapp.service.TransactionService;
 import vn.easyca.signserver.webapp.service.dto.TransactionDTO;
@@ -181,107 +182,4 @@ public class SignController {
     }
 }
 
-class CustomToolKit extends HTMLEditorKit {
 
-    private static HTMLFactory factory = null;
-
-    @Override
-    public ViewFactory getViewFactory() {
-        if (factory == null) {
-            factory = new HTMLFactory() {
-
-                @Override
-                public View create(Element elem) {
-                    AttributeSet attrs = elem.getAttributes();
-                    Object elementName = attrs.getAttribute(AbstractDocument.ElementNameAttribute);
-                    Object o = (elementName != null) ? null : attrs.getAttribute(StyleConstants.NameAttribute);
-                    if (o instanceof HTML.Tag) {
-                        HTML.Tag kind = (HTML.Tag) o;
-                        if (kind == HTML.Tag.IMG) {
-                            return new BASE64ImageView(elem);
-                        }
-                    }
-                    return super.create(elem);
-                }
-            };
-        }
-        return factory;
-    }
-
-}
-
-class BASE64ImageView extends ImageView {
-
-    private URL url;
-
-    public BASE64ImageView(Element elmnt) {
-        super(elmnt);
-        populateImage();
-    }
-
-    private void populateImage() {
-        Dictionary<URL, Image> cache = (Dictionary<URL, Image>) getDocument()
-            .getProperty("imageCache");
-        if (cache == null) {
-            cache = new Hashtable<>();
-            getDocument().putProperty("imageCache", cache);
-        }
-
-        URL src = getImageURL();
-        cache.put(src, loadImage());
-
-    }
-
-    private Image loadImage() {
-        String b64 = getBASE64Image();
-        BufferedImage newImage = null;
-        ByteArrayInputStream bais = null;
-        try {
-            bais = new ByteArrayInputStream(
-                Base64.getDecoder().decode(b64.getBytes()));
-            newImage = ImageIO.read(bais);
-        } catch (Throwable ex) {
-            ex.printStackTrace();
-        }
-        return newImage;
-    }
-
-    @Override
-    public URL getImageURL() {
-        String src = (String) getElement().getAttributes()
-            .getAttribute(HTML.Attribute.SRC);
-        if (isBase64Encoded(src)) {
-
-            this.url = BASE64ImageView.class.getProtectionDomain()
-                .getCodeSource().getLocation();
-
-            return this.url;
-        }
-        return super.getImageURL();
-    }
-
-    private boolean isBase64Encoded(String src) {
-        return src != null && src.contains("base64,");
-    }
-
-    private String getBASE64Image() {
-        String src = (String) getElement().getAttributes()
-            .getAttribute(HTML.Attribute.SRC);
-        if (!isBase64Encoded(src)) {
-            return null;
-        }
-        return src.substring(src.indexOf("base64,") + 7, src.length() - 1);
-    }
-
-}
-
-class HtmlImageGeneratorCustom extends HtmlImageGenerator {
-    @Override
-    protected JEditorPane createJEditorPane() {
-        JEditorPane edit = super.createJEditorPane();
-        edit.setEditable(false);
-        CustomToolKit toolKit = new CustomToolKit();
-        edit.setEditorKit(toolKit);
-        return edit;
-    }
-}
