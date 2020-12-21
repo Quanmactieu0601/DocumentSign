9d485b9f1586bb9628d61b0d99c60753c6b38d4c
ThanhLD <duythanh.h3t@gmail.com>
2020-12-21 09:48:18 +0700

    update

--- db-change-log/initial-data.sql
+++ db-change-log/initial-data.sql
@@ -3,7 +3,6 @@ INSERT INTO signserver2.jhi_authority (name, id) VALUES ('ROLE_ADMIN', null);
 INSERT INTO signserver2.jhi_authority (name, id) VALUES ('ROLE_USER', null);
 
 --  create admin user account
---admin | admin@123$%
 INSERT INTO jhi_user (id, created_by, created_date, last_modified_by, last_modified_date, activated, activation_key, email, first_name, image_url, lang_key, last_name, login, password_hash, reset_date, reset_key, organization_name, common_name, organization_unit, locality_name, state_name, country, phone, csr_status, com_id) VALUES (1, 'thanhld', '2020-12-21 09:20:44.331000', 'thanhld', '2020-12-21 09:20:44.331000', true, '779ocAqcgQ5BpZHxJ5Y1', 'admin-signserver@softdreams.vn', 'Super Admin', null, 'vi', null, 'admin', '$2a$10$bJ.4IDe6SnyqHfcekn/Nx..4vObeRDb3eti6GcjNsAjT8xxauavg6', null, null, null, null, null, null, null, null, null, 0, 1);
 
 -- grant permission to admin user
