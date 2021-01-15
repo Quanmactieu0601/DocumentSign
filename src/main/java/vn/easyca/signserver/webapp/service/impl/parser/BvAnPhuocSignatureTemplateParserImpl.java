db37b8ac0c330b4116e916d08590c39a01a48dea
tungpt <tungpt@softdreams.vn>
2021-01-15 08:41:52 +0700

    add parse to bv Binh Phuoc

--- src/main/java/vn/easyca/signserver/webapp/service/impl/parser/BvAnPhuocSignatureTemplateParserImpl.java
+++ src/main/java/vn/easyca/signserver/webapp/service/impl/parser/BvAnPhuocSignatureTemplateParserImpl.java
@@ -2,12 +2,27 @@ package vn.easyca.signserver.webapp.service.impl.parser;
 
 import org.springframework.stereotype.Service;
 import vn.easyca.signserver.webapp.service.parser.SignatureTemplateParseService;
+import vn.easyca.signserver.webapp.utils.DateTimeUtils;
+import vn.easyca.signserver.webapp.utils.ParserUtils;
 
 @Service
 public class BvAnPhuocSignatureTemplateParserImpl implements SignatureTemplateParseService {
 
     @Override
     public String buildSignatureTemplate(String subjectDN, String signatureTemplate, String signatureImage) {
-        return null;
+        final String regexCN = "CN=\"([^\"]+)\"";
+        final String regexT = ", T=([^,]+)";
+        String CN = ParserUtils.getElementContentNameInCertificate(subjectDN, regexCN);
+        String T = ParserUtils.getElementContentNameInCertificate(subjectDN, regexT);
+        String[] signerInfor = CN.split(",");
+        String signerName = signerInfor[0];
+
+        String htmlContent = signatureTemplate;
+        htmlContent = htmlContent
+            .replaceFirst("signer", signerName)
+            .replaceFirst("position", T)
+            .replaceFirst("signatureImage", signatureImage)
+            .replaceFirst("timeSign", DateTimeUtils.getCurrentTimeStampWithFormat(DateTimeUtils.HHmmss_ddMMyyyy));
+        return htmlContent;
     }
 }
