b6d9fc340db8de40e9b0c712191d703c0f5eda28
ThanhLD <thanhld@softdreams.vn>
2021-03-30 15:15:04 +0700

    update dbchangelog + prod db profile

--- db-change-log/mariadb/20210227-add-core_parser_table.sql
+++ db-change-log/mariadb/20210227-add-core_parser_table.sql
@@ -4,3 +4,6 @@ create table core_parser
 	name varchar(100) null,
 	description varchar(500) null
 );
+
+INSERT INTO core_parser (id, name, description) VALUES (1, 'BV_Q11', null);
+INSERT INTO core_parser (id, name, description) VALUES (2, 'BV_AnPhuoc', null);
