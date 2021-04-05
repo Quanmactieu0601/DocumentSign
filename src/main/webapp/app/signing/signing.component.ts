f75c9e84bf9ab643e680b3d83325982174de57f8
tungpt <tungpt@softdreams.vn>
2021-04-05 13:54:44 +0700

    update branch

---
+++ src/main/webapp/app/signing/signing.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'jhi-signing',
+  templateUrl: './signing.component.html',
+  styleUrls: ['./signing.component.scss'],
+})
+export class SigningComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}
