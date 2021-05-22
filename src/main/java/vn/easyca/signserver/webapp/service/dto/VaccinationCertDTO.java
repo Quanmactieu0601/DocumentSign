6aa17461e524fc154230c0c4a8a9e83071f30cfc
tungpt <tungpt@softdreams.vn>
2021-05-22 13:55:34 +0700

    update new library convert to image

--- src/main/java/vn/easyca/signserver/webapp/service/dto/VaccinationCertDTO.java
+++ src/main/java/vn/easyca/signserver/webapp/service/dto/VaccinationCertDTO.java
@@ -3,6 +3,7 @@ package vn.easyca.signserver.webapp.service.dto;
 import com.itextpdf.text.BadElementException;
 import jdk.nashorn.internal.ir.ReturnNode;
 import org.apache.commons.io.IOUtils;
+import org.springframework.core.env.Environment;
 import vn.easyca.signserver.core.domain.TokenInfo;
 import vn.easyca.signserver.core.dto.sign.TokenInfoDTO;
 import vn.easyca.signserver.core.dto.sign.newrequest.ExtraInfo;
@@ -11,10 +12,13 @@ import vn.easyca.signserver.core.dto.sign.newrequest.SigningRequest;
 import vn.easyca.signserver.core.dto.sign.newrequest.VisibleRequestContent;
 import vn.easyca.signserver.core.exception.ApplicationException;
 import vn.easyca.signserver.webapp.service.FileResourceService;
+import vn.easyca.signserver.webapp.utils.ParserUtils;
 
 import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Base64;
+import java.util.Date;
 import java.util.List;
 
 public class VaccinationCertDTO {
@@ -22,33 +26,49 @@ public class VaccinationCertDTO {
     private String serial;
     private String pin;
 
-    public String getSerial() { return serial; }
+    public String getSerial() {
+        return serial;
+    }
 
-    public void setSerial(String serial) { this.serial = serial; }
+    public void setSerial(String serial) {
+        this.serial = serial;
+    }
 
-    public String getPin() { return pin; }
+    public String getPin() {
+        return pin;
+    }
 
-    public void setPin(String pin) { this.pin = pin; }
+    public void setPin(String pin) {
+        this.pin = pin;
+    }
 
-    public byte[] getConfirmContentFile() { return confirmContentFile; }
+    public byte[] getConfirmContentFile() {
+        return confirmContentFile;
+    }
 
-    public void setConfirmContentFile(byte[] contentFile) { confirmContentFile = contentFile; }
+    public void setConfirmContentFile(byte[] contentFile) {
+        confirmContentFile = contentFile;
+    }
 
-    public SigningRequest<VisibleRequestContent> createSigningRequest(FileResourceService fileResourceService) throws IOException, ApplicationException, BadElementException {
+    public SigningRequest<VisibleRequestContent> createSigningRequest(FileResourceService fileResourceService, Environment env) throws IOException, ApplicationException, BadElementException {
         SigningRequest<VisibleRequestContent> signingRequest = new SigningRequest<>();
         List<VisibleRequestContent> visibleRequestContentList = new ArrayList<VisibleRequestContent>();
         VisibleRequestContent visibleRequestContent = new VisibleRequestContent();
-        visibleRequestContent.setImageSignature(getSignatureImage(fileResourceService));
+
+        String htmlContentImage = getSignatureImage(fileResourceService);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss");
+        Date date = new Date();
+        String fullDate = formatter.format(date) + " +07'00'";
+        htmlContentImage = htmlContentImage.replace("Time", fullDate);
+
+        String contentImage = ParserUtils.convertHtmlContentToImageByProversion(htmlContentImage, 718, 295, false, env);
+        visibleRequestContent.setImageSignature(contentImage);
 
         Location location = new Location();
-//        location.setVisibleHeight(112);
-//        location.setVisibleWidth(150);
-//        location.setVisibleX(225);
-//        location.setVisibleY(53);
-        location.setVisibleHeight(132);
-        location.setVisibleWidth(170);
-        location.setVisibleX(360);
-        location.setVisibleY(105);
+        location.setVisibleHeight(130);
+        location.setVisibleWidth(307);
+        location.setVisibleX(225);
+        location.setVisibleY(110);
 
         visibleRequestContent.setLocation(location);
         visibleRequestContent.setExtraInfo(new ExtraInfo());
@@ -66,6 +86,6 @@ public class VaccinationCertDTO {
     }
 
     private String getSignatureImage(FileResourceService fileResourceService) throws ApplicationException, IOException {
-        return Base64.getEncoder().encodeToString(IOUtils.toByteArray(fileResourceService.getTemplateFile("/templates/signature/VaccinationCertImage.png")));
+        return new String(IOUtils.toByteArray(fileResourceService.getTemplateFile("/templates/signature/vaccinationCertImageHtml.html")));
     }
 }
