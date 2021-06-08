aa79c5aeffdfcebaa0a8baef38b70b14fe3e1033
ThanhLD <thanhld@softdreams.vn>
2021-06-08 19:50:47 +0700

    update

--- db-change-log/postgres/20210118-add-system_config_category.sql
+++ db-change-log/postgres/20210118-add-system_config_category.sql
@@ -13,3 +13,8 @@ alter table system_config_category
 	add constraint system_config_category_pk
 		primary key (id);
 
+
+INSERT INTO system_config_category (id, config_key, data_type, description) VALUES (1, 'SYMMETRIC_KEY', 'STRING', 'Khóa mã hóa các dữ liệu đặc biệt');
+INSERT INTO system_config_category (id, config_key, data_type, description) VALUES (2, 'SAVE_TOKEN_PASSWORD', 'BOOLEAN', 'Lưu mật khẩu P12 (1: có lưu | 0: không lưu)');
+INSERT INTO system_config_category (id, config_key, data_type, description) VALUES (3, 'USE_OTP', 'BOOLEAN', 'Sử dụng OTP để xác thực mỗi khi ký số (1: có sử dụng | 0: không sử dụng)');
+INSERT INTO system_config_category (id, config_key, data_type, description) VALUES (4, 'OTP_LIFE_TIME_SECOND', 'NUMERIC', 'Lifetime của OTP (đơn vị: giây - Min 60s)');
---
+++ db-change-log/postgres/20210324-modify-signature_template.sql
@@ -0,0 +1,2 @@
+alter table signature_template
+	add transparency boolean default false;
