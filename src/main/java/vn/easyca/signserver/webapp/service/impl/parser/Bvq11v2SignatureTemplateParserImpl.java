a333c6acadb6404448642435897e07b62bf60512
DESKTOP-H4FLVMV\Windows 10 YSL 1909 <leanhduc944@gmail.com>
2021-06-25 17:01:19 +0700

    change signature image

---
+++ src/main/java/vn/easyca/signserver/webapp/service/impl/parser/Bvq11v2SignatureTemplateParserImpl.java
@@ -0,0 +1,40 @@
+package vn.easyca.signserver.webapp.service.impl.parser;
+
+import org.springframework.stereotype.Service;
+import vn.easyca.signserver.core.exception.ApplicationException;
+import vn.easyca.signserver.webapp.service.parser.SignatureTemplateParseService;
+import vn.easyca.signserver.webapp.utils.DateTimeUtils;
+import vn.easyca.signserver.webapp.utils.ParserUtils;
+
+@Service
+public class Bvq11v2SignatureTemplateParserImpl implements SignatureTemplateParseService {
+    final String regexCN = "CN=\"([^\"]+)\"";
+    @Override
+    public String buildSignatureTemplate(String subjectDN, String signatureTemplate, String signatureImage) throws ApplicationException {
+        try {
+            final String regexT = ", T=([^,]+";
+            String CN = getSigner(subjectDN);
+            String T = ParserUtils.getElementContentNameInCertificate(subjectDN, regexCN);
+            String[] signerInfor = CN.split(",");
+            String signerName = signerInfor[0];
+            String address = signerInfor[1];
+
+            String htmlContent = signatureTemplate;
+            htmlContent = htmlContent
+                .replaceFirst("_signer_", signerName)
+                .replaceFirst("_position", T)
+                .replaceFirst("_address_", address)
+                .replaceFirst("_signatureImage_", signatureImage)
+                .replaceFirst("_timeSign", DateTimeUtils.getCurrentTimeStampWithFormat(DateTimeUtils.HHmmss_ddMMyyyy));
+            return htmlContent;
+        } catch (Exception ex) {
+            throw new ApplicationException(String.format("Error when build template: subjectDN: %s -ex: %s", subjectDN, ex.getMessage()), ex);
+        }
+    }
+
+    @Override
+    public String getSigner(String subjectDN) throws ApplicationException {
+        return ParserUtils.getElementContentNameInCertificate(subjectDN, regexCN);
+    }
+
+}
