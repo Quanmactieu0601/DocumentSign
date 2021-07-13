0da55731c5c51ff86edf7499d1c00ab5b65df3ed
tungpt <tungpt@softdreams.vn>
2021-07-13 19:44:53 +0700

    change code for iostream

--- src/main/java/vn/easyca/signserver/core/services/SigningService.java
+++ src/main/java/vn/easyca/signserver/core/services/SigningService.java
@@ -54,7 +54,7 @@ public class SigningService {
             file.mkdir();
     }
 
-    public PDFSigningDataRes signPDFFile(SigningRequest request) throws ApplicationException {
+    public PDFSigningDataRes signPDFFile(SigningRequest request) throws ApplicationException, IOException {
         CertificateDTO certificateDTO = certificateService.getBySerial(request.getTokenInfo().getSerial());
         if (certificateDTO == null)
             throw new CertificateNotFoundAppException();
