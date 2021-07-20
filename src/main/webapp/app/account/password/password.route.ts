6adb698dc841403666bef6e6056a46c96702a422
linhntt1103 <love113@outlook.com.vn>
2021-07-20 10:18:10 +0700

    fix change role of setting account and pass

--- src/main/webapp/app/account/password/password.route.ts
+++ src/main/webapp/app/account/password/password.route.ts
@@ -8,7 +8,7 @@ export const passwordRoute: Route = {
   path: 'password',
   component: PasswordComponent,
   data: {
-    authorities: [Authority.USER],
+    authorities: [Authority.USER, Authority.SUPER_ADMIN, Authority.ADMIN, Authority.SIGN, Authority.VERIFY],
     pageTitle: 'global.menu.account.password',
   },
   canActivate: [UserRouteAccessService],
