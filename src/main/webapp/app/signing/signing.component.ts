fdd5fb854fd7cc2fe4c32f81dd5f3cd309592ba2
tungpt <tungpt@softdreams.vn>
2021-07-23 11:59:06 +0700

    update code

--- src/main/webapp/app/signing/signing.component.ts
+++ src/main/webapp/app/signing/signing.component.ts
@@ -1,5 +1,19 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { Component, ElementRef, EventEmitter, HostListener, OnInit, Output, ViewChild } from '@angular/core';
 import { DomSanitizer } from '@angular/platform-browser';
+import { ICertificate } from 'app/shared/model/certificate.model';
+import { Subscription } from 'rxjs';
+import { Account } from 'app/core/user/account.model';
+import { AccountService } from 'app/core/auth/account.service';
+import { ToastrService } from 'ngx-toastr';
+import { TranslateService } from '@ngx-translate/core';
+import { SigningService } from 'app/signing/signing.service';
+import { FormBuilder, Validators } from '@angular/forms';
+import { CertificateService } from 'app/entities/certificate/certificate.service';
+import { HttpResponse } from '@angular/common/http';
+import { saveAs } from 'file-saver';
+import { NgbModal, NgbModalRef } from '@ng-bootstrap/ng-bootstrap';
+import { SignatureListComponent } from 'app/signing/pdf-view/signature-list/signature-list.component';
+import { ResponseBody } from 'app/shared/model/response-body';
 
 @Component({
   selector: 'jhi-signing',
@@ -7,16 +21,201 @@ import { DomSanitizer } from '@angular/platform-browser';
   styleUrls: ['./signing.component.scss'],
 })
 export class SigningComponent implements OnInit {
+  @ViewChild('serialElement') serialElement: ElementRef | undefined;
+  @ViewChild('signatureImage') signatureImage: ElementRef | undefined;
+  @ViewChild('nextBtnElement') nexBtnElement: ElementRef | undefined;
+  @ViewChild('wizzard') wizzard: any;
+  @Output() nextActionEvent = new EventEmitter<any>();
+  isShowMessage = false;
+  // signingForm = this.fb.group({
+  //   serial: ['', Validators.required],
+  //   pin: ['', Validators.required],
+  //   templateId: ['', Validators.required],
+  // });
+
   FileToSign: any = null;
   srcPdfResult: any;
   imageSrc: any;
   serial: any;
   pin: any;
 
-  @ViewChild('wizzard') wizzard: any;
-  constructor(private sanitizer: DomSanitizer) {}
+  showMessageSerialRequired = false;
+  modalRef: NgbModalRef | undefined;
 
-  ngOnInit(): void {}
+  selectFiles: File[] = [];
+  currentFile?: File;
+  listCertificate: ICertificate[] = [];
+  authSubscription?: Subscription;
+  account: Account | null = null;
+  fileName: string | undefined;
+  resFile = '';
+  page = 0;
+  timer: NodeJS.Timeout | undefined;
+  editForm = this.fb.group({
+    serial: ['', Validators.required],
+    pin: ['', Validators.required],
+    templateId: ['', Validators.required],
+    fileSelect: ['', Validators.required],
+  });
+  constructor(
+    private accountService: AccountService,
+    private toastrService: ToastrService,
+    private translateService: TranslateService,
+    private signingService: SigningService,
+    private fb: FormBuilder,
+    private certificateService: CertificateService,
+    private el: ElementRef,
+    private modalService: NgbModal
+  ) {}
+
+  ngOnInit(): void {
+    this.authSubscription = this.accountService.getAuthenticationState().subscribe(account => (this.account = account));
+    this.getListCertificate('', 0);
+  }
+
+  getListCertificate(s: string, p: number): void {
+    const data = {
+      page: p,
+      size: 20,
+      sort: ['id,desc'],
+      alias: null,
+      ownerId: this.account?.login,
+      serial: s,
+      validDate: null,
+      expiredDate: null,
+    };
+    if (p === 0) this.listCertificate = [];
+    this.certificateService.findCertificate(data).subscribe((res: HttpResponse<ICertificate[]>) => {
+      this.listCertificate.push(...(res.body || []));
+    });
+  }
+
+  @HostListener('scroll', ['$event'])
+  getMoreCert(e: any): void {
+    if (e.target.scrollHeight === e.target.scrollTop + e.target.clientHeight) {
+      this.getListCertificate(this.serial, ++this.page);
+    }
+  }
+
+  selectSerial(serial: string): void {
+    this.serial = serial;
+  }
+
+  filter(part: string): void {
+    if (this.timer) {
+      clearTimeout(this.timer);
+    }
+    this.timer = setTimeout(() => {
+      this.page = 0;
+      this.getListCertificate(part, this.page);
+    }, 1000);
+  }
+
+  selectFile(event: any): void {
+    if (this.selectFiles.length !== 0) this.removeFile(event);
+    this.selectFiles.push(...event.addedFiles);
+    this.editForm.controls['fileSelect'].setValue(1);
+    this.editForm.controls['fileSelect'].markAllAsTouched();
+    this.validateFileInput(event.addedFiles[0]);
+  }
+
+  removeFile(event: any): void {
+    this.selectFiles.splice(this.selectFiles.indexOf(event), 1);
+    this.editForm.controls['fileSelect'].setValue('');
+    this.setDisableButton();
+  }
+
+  signDoc(): void {
+    this.currentFile = this.selectFiles[0];
+    const reader = new FileReader();
+    reader.readAsArrayBuffer(this.currentFile);
+    reader.onload = () => {
+      const request = {
+        signingRequestContents: [
+          {
+            data: this.arrayBufferToBase64(reader.result),
+            documentName: '123',
+          },
+        ],
+        tokenInfo: {
+          pin: this.editForm.get(['pinCode'])!.value,
+          // serial: this.editForm.get(['serial'])!.value,
+          serial: this.serial,
+        },
+        optional: {
+          otpCode: '621143',
+        },
+      };
+      this.signingService.signDocInvisible(request).subscribe((res: any) => {
+        if (JSON.parse(res).status === -1) this.toastrService.error(JSON.parse(res).msg);
+        this.resFile = JSON.parse(res).data.responseContentList[0].signedDocument;
+        const byteArray = this.base64ToArrayBuffer(this.resFile);
+        saveAs(
+          new Blob([byteArray], { type: 'application/vnd.openxmlformats-officedocument.wordprocessingml.document' }),
+          Date.now().toString()
+        );
+      });
+    };
+  }
+
+  arrayBufferToBase64(buffer: any): string {
+    return btoa(new Uint8Array(buffer).reduce((data, byte) => data + String.fromCharCode(byte), ''))
+      .toString()
+      .replace('data:application/pdf;base64,', '');
+  }
+
+  base64ToArrayBuffer(base64: any): ArrayBuffer {
+    const binaryString = window.atob(base64);
+    const len = binaryString.length;
+    const bytes = new Uint8Array(len);
+    for (let i = 0; i < len; i++) {
+      bytes[i] = binaryString.charCodeAt(i);
+    }
+    return bytes.buffer;
+  }
+
+  openModalTemplateList(): void {
+    this.modalRef = this.modalService.open(SignatureListComponent, { size: 'md' });
+    this.modalRef.result.then(templateId => {
+      this.editForm.controls['templateId'].setValue(templateId);
+    });
+    this.accountService.identity(false).subscribe(res => {
+      this.modalRef!.componentInstance.userId = res?.id;
+    });
+  }
+
+  checkValidatedImage(): void {
+    for (const key of Object.keys(this.editForm.controls)) {
+      if (this.editForm.controls[key].invalid) {
+        const invalidControl = this.el.nativeElement.querySelector('[formcontrolname="' + key + '"]');
+        invalidControl.focus();
+        this.isShowMessage = true;
+        return;
+      }
+    }
+
+    const data = {
+      ...this.editForm.value,
+    };
+
+    this.certificateService.getSignatureImageByTemplateId(data).subscribe((res: ResponseBody) => {
+      if (res.status === ResponseBody.SUCCESS) {
+        this.toastrService.success(this.translateService.instant('sign.messages.validate.validated'));
+        this.signatureImage && this.signatureImage.nativeElement
+          ? (this.signatureImage.nativeElement.src = 'data:image/jpeg;base64,' + res.data)
+          : null;
+        this.nexBtnElement!.nativeElement.disabled = false;
+        this.isShowMessage = true;
+      } else {
+        this.toastrService.error(res.msg);
+      }
+    });
+  }
+
+  nextAction(): void {
+    this.imageSrc = this.signatureImage?.nativeElement.src;
+    this.wizzard.goToNextStep();
+  }
 
   validateFileInput(FileToSign: File): any {
     if (typeof FileReader !== 'undefined') {
@@ -33,6 +232,7 @@ export class SigningComponent implements OnInit {
   cancel(): void {
     this.wizzard.goToPreviousStep();
   }
+
   signResult(signedFile: any): void {
     if (signedFile) {
       this.wizzard.goToNextStep();
@@ -40,10 +240,9 @@ export class SigningComponent implements OnInit {
     }
   }
 
-  nextStep(content: any): void {
-    this.imageSrc = content.signatureImage;
-    this.serial = content.signingForm.get('serial').value;
-    this.pin = content.signingForm.get('pin').value;
-    this.wizzard.goToNextStep();
+  nextStep(content: any): void {}
+
+  setDisableButton(): void {
+    this.nexBtnElement!.nativeElement.disabled = true;
   }
 }
