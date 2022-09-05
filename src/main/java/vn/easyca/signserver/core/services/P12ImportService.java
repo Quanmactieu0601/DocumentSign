352a9af50a5b76a1f94e0ab493a1c0023743eee6
tungpt <tungphamhttt1@gmail.com>
2022-09-05 08:23:06 +0700

    cập nhật code

--- src/main/java/vn/easyca/signserver/core/services/P12ImportService.java
+++ src/main/java/vn/easyca/signserver/core/services/P12ImportService.java
@@ -1,5 +1,6 @@
 package vn.easyca.signserver.core.services;
 
+import com.google.common.base.Strings;
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
 import org.apache.poi.ss.usermodel.DataFormatter;
@@ -13,6 +14,8 @@ import vn.easyca.signserver.core.domain.CertificateDTO;
 import vn.easyca.signserver.core.dto.ImportP12FileDTO;
 import vn.easyca.signserver.core.exception.ApplicationException;
 import vn.easyca.signserver.core.exception.CertificateAppException;
+import vn.easyca.signserver.core.factory.CryptoTokenProxyFactory;
+import vn.easyca.signserver.pki.cryptotoken.CryptoToken;
 import vn.easyca.signserver.pki.cryptotoken.impl.P12CryptoToken;
 import vn.easyca.signserver.core.domain.TokenInfo;
 import vn.easyca.signserver.core.utils.CertUtils;
@@ -21,6 +24,7 @@ import vn.easyca.signserver.webapp.config.SystemDbConfiguration;
 import vn.easyca.signserver.webapp.domain.Certificate;
 import vn.easyca.signserver.webapp.domain.UserEntity;
 import vn.easyca.signserver.webapp.repository.CertificateRepository;
+import vn.easyca.signserver.webapp.repository.UserRepository;
 import vn.easyca.signserver.webapp.security.AuthenticatorTOTPService;
 import vn.easyca.signserver.webapp.service.CertificateService;
 import vn.easyca.signserver.webapp.service.SystemConfigCachingService;
@@ -30,13 +34,11 @@ import vn.easyca.signserver.webapp.utils.DateTimeUtils;
 import vn.easyca.signserver.webapp.utils.MappingHelper;
 import vn.easyca.signserver.webapp.utils.SymmetricEncryptors;
 
-import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.security.*;
 import java.security.cert.CertificateEncodingException;
-import java.security.cert.CertificateException;
 import java.security.cert.X509Certificate;
 import java.util.*;
 
@@ -50,16 +52,19 @@ public class P12ImportService {
     private final CertificateRepository certificateRepository;
     private final SystemConfigCachingService systemConfigCachingService;
     private final AuthenticatorTOTPService authenticatorTOTPService;
-
+    private final CertificateGenerateService certificateGenerateService;
+    private final CryptoTokenProxyFactory cryptoTokenProxyFactory;
     @Autowired
     public P12ImportService(CertificateService certificateService, UserApplicationService userApplicationService,
-                            SymmetricEncryptors symmetricService, CertificateRepository certificateRepository, SystemConfigCachingService systemConfigCachingService, AuthenticatorTOTPService authenticatorTOTPService) {
+                            SymmetricEncryptors symmetricService, CertificateRepository certificateRepository, SystemConfigCachingService systemConfigCachingService, AuthenticatorTOTPService authenticatorTOTPService, CertificateGenerateService certificateGenerateService, UserRepository userRepository, CryptoTokenProxyFactory cryptoTokenProxyFactory) {
         this.certificateService = certificateService;
         this.userApplicationService = userApplicationService;
         this.symmetricService = symmetricService;
         this.certificateRepository = certificateRepository;
         this.systemConfigCachingService = systemConfigCachingService;
         this.authenticatorTOTPService = authenticatorTOTPService;
+        this.certificateGenerateService = certificateGenerateService;
+        this.cryptoTokenProxyFactory = cryptoTokenProxyFactory;
     }
 
     public CertificateDTO insert(ImportP12FileDTO input) throws ApplicationException {
@@ -124,10 +129,9 @@ public class P12ImportService {
     }
 
     public CertificateDTO insertP12(ImportP12FileDTO input) throws ApplicationException {
-
         Optional<UserEntity> userEntity = userApplicationService.getUserWithAuthoritiesByLogin(input.getOwnerId());
         boolean check = userEntity.isPresent();
-        if(!check) throw new ApplicationException("Don't have valid account");
+        if (!check) throw new ApplicationException("Don't have valid account");
 
         P12CryptoToken p12CryptoToken = new P12CryptoToken();
         try {
@@ -206,21 +210,30 @@ public class P12ImportService {
             for (int i = 2; i < rows; i++) {
                 Row row = sheet.getRow(i);
                 if (row != null) {
-                    String pin = formatter.formatCellValue(row.getCell(18, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK));
-                    String base64Certificate = formatter.formatCellValue(row.getCell(15, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)).replaceAll("\n", "");;
-                    Optional<UserEntity> userId = userApplicationService.getUserWithAuthorities();
-                    ImportP12FileDTO p12ImportVM = new ImportP12FileDTO();
-                    p12ImportVM.setOwnerId(userId.get().getLogin());
-                    p12ImportVM.setPin(pin);
-                    p12ImportVM.setP12Base64(base64Certificate);
-                    ImportP12FileDTO serviceInput = MappingHelper.map(p12ImportVM, ImportP12FileDTO.class);
+                    boolean isP12 = Strings.isNullOrEmpty(row.getCell(14).getStringCellValue());
+                    if (isP12) {
+                        String pin = formatter.formatCellValue(row.getCell(18, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK));
+                        String base64Certificate = formatter.formatCellValue(row.getCell(15, Row.MissingCellPolicy.CREATE_NULL_AS_BLANK)).replaceAll("\n", "");
+                        Optional<UserEntity> userId = userApplicationService.getUserWithAuthorities();
+                        ImportP12FileDTO p12ImportVM = new ImportP12FileDTO();
+                        p12ImportVM.setOwnerId(userId.get().getLogin());
+                        p12ImportVM.setPin(pin);
+                        p12ImportVM.setP12Base64(base64Certificate);
+                        ImportP12FileDTO serviceInput = MappingHelper.map(p12ImportVM, ImportP12FileDTO.class);
 
-                    try {
-                        CertificateDTO certificateDTO = this.insertP12(serviceInput);
-                        row.createCell(19).setCellValue("Imported successfully ");
-                    } catch (Exception ex) {
-                        row.createCell(19).setCellValue("Imported error");
-                        row.createCell(20).setCellValue(ex.getMessage());
+                        try {
+                            CertificateDTO certificateDTO = this.insertP12(serviceInput);
+                            row.createCell(19).setCellValue("Imported successfully ");
+                        } catch (Exception ex) {
+                            row.createCell(19).setCellValue("Imported error");
+                            row.createCell(20).setCellValue(ex.getMessage());
+                        }
+                    } else {
+                        String certValue = row.getCell(15).getStringCellValue();
+                        String alias = row.getCell(13).getStringCellValue();
+                        String ownerId = userApplicationService.getUserWithAuthorities().get().getLogin();
+                        CryptoToken cryptoToken = cryptoTokenProxyFactory.resolveP11Token(null);
+                        certificateGenerateService.saveAndInstallCert(certValue, alias, ownerId, cryptoToken);
                     }
                 }
             }
